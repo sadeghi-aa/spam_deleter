b6b332e952e6213c61bbf4d8df0324f31d74a0db
Amirali <34868015+sadeghi-aa@users.noreply.github.com>
2021-05-07 17:09:08 +0430

    Handle non-reply commands

--- spam_deleter.py
+++ spam_deleter.py
@@ -4,7 +4,7 @@ import telebot
 
 app = Flask(__name__)
 
-token = "<Your Bot Token>"
+token = "1854811438:AAGiriL0MQAGZsGHYzLvDs7AbkI829u3htM"
 bot = telebot.TeleBot(token, threaded=False)
 
 warning_text = '\n'.join([
@@ -29,16 +29,20 @@ def delete(message):
                 replied_to = bot.get_chat_member(chat_id, message.reply_to_message.from_user.id)
                 if replied_to.status == 'member':
                     spam_id = message.reply_to_message.message_id
+                    spammer_id = replied_to.user.id
                     spammer_name = message.reply_to_message.from_user.first_name
                     command_id = message.message_id
                     bot.delete_message(chat_id, spam_id)
                     bot.delete_message(chat_id, command_id)
-                    mention = "[" + spammer_name + "](tg://user?id="+str(user_id)+")"
+                    mention = "[" + spammer_name + "](tg://user?id="+str(spammer_id)+")"
                     text = '\n'.join([mention, warning_text])
                     bot.send_message(chat_id, text, parse_mode="Markdown")
                 else: # Tried to delete an admin's message
                     command_id = message.message_id
                     bot.delete_message(chat_id, command_id)
+            else: # Not replied on anything
+                command_id = message.message_id
+                bot.delete_message(chat_id, command_id)
         else: # Not an admin
             command_id = message.message_id
             bot.delete_message(chat_id, command_id)
